9d52e3c49e3073c689c71852a9768ea665f75a57
rsilva92 <contato.rafaeloliveiradasilva@gmail.com>
2024-09-30 11:42:20 -0400

    primeiro commit sobre estudo php

---
+++ sessao-01/aula06-OperadoresAritimeticos.php
@@ -0,0 +1,35 @@
+<?php
+/**
+aula de operadores aritiméticos
+
+**/
+
+$numero01 = 100;
+$numero02 = 100;
+
+
+//somar
+$somar = $numero01 + $numero02;
+echo '</br>';
+echo $somar;
+
+//subtrair
+$subtrair = $numero01 - $numero02;
+echo '</br>';
+echo $subtrair;
+
+//multiplicar
+$multiplicar = $numero01 * $numero02;
+echo '</br>';
+echo $multiplicar;
+
+//divisão
+$divisao = $numero01 / $numero02;                 
+echo '</br>';
+echo $divisao;
+
+//resto
+$resto = $numero01 % $numero02;
+echo '</br>';
+echo $resto;
+?>
